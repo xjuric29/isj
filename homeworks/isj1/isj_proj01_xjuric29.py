54e5ded8865a5dc740c747e20bd9bb45bcccbd59
xjuric29 <xjuric29@stud.fit.vutbr.cz>
2017-03-06 22:02:35 +0100

    Merging with complete wis version

--- homeworks/isj1/isj_proj01_xjuric29.py
+++ homeworks/isj1/isj_proj01_xjuric29.py
@@ -6,7 +6,7 @@ import re
 def first_task():
     # trivialni ukol 1 - 1 bod
     objects = 'Mars Jupiter Uran Neptun Pluto'
-    tests1 = ['Mars Jupiter Uran Neptun Pluto', 'Jupiter Uran Neptun', 'Jupiter', 'Mars', 'Neptun', 'Uran']
+    tests1 = ['Neptun']
     for test in tests1:
         if re.search(r'^.*\b(Jupiter|Mars|Neptun|Uran)\b.*$', objects).group(1) == test:
 
@@ -18,7 +18,7 @@ def first_task():
 
 def second_task():
     # trivialni ukol 2 - 1 bod
-    tests2 = ['999==666', '987==987', '765==765', '666==666']
+    tests2 = ['987==987', '666==666']
     for test in tests2:
         if re.search(r'([987]{3}|[654]{3})==\1', test):
             assert True
@@ -28,8 +28,8 @@ def second_task():
 
 def third_task():
     # mene trivialni ukol - 3 body
-    punct = re.compile(r'''(moje reseni  # after a full stop or comma
-                            (vyuziva     # ...
+    punct = re.compile(r'''(?<=[,.]  # after a full stop or comma
+                            (?![\d]     # ...
                             ))''', re.X)
     assert punct.sub(' ', 'Hello,John.I bought 192.168.0.1 for 100,000 bitcoins') == 'Hello, John. I bought 192.168.0.1 for 100,000 bitcoins'
 
