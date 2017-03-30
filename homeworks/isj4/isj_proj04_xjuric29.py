861128ee59cfda94214418a12c914696e53ba0f1
xjuric29 <xjuric29@stud.fit.vutbr.cz>
2017-03-30 17:26:27 +0200

    Second func complete, first revision in project 4

--- homeworks/isj4/isj_proj04_xjuric29.py
+++ homeworks/isj4/isj_proj04_xjuric29.py
@@ -1,28 +1,39 @@
 #!/usr/bin/env python3
 
+import string
+
 def balanced_paren (parenstr):
-	brackets = []
-	lenght = len (parenstr)
-	count = 1
+	tmp = []
+	brackets = { ')':'(', ']':'[', '}':'{', '>':'<'}
+	length = len (parenstr)
 	for char in parenstr:
-		if char in "([{<":
-			brackets.append (char)
-		if char in "]}>":
-			if count <= lenght and ord (brackets[-count]) == (ord (char) - 2):
-				count += 1
+		if char in "([{<":		# If char is beginnig brackets store it in tmp
+			tmp.append (char)
+		if char in ")]}>":
+			if tmp != [] and tmp[-1] == brackets[char]:		
+				tmp = tmp[:-1]
 			else:
 				return False
-		if char == ')':
-                        if count <= lenght and ord (brackets[-count]) == (ord (char) - 1):
-                                count += 1
-                        else:
-                                return False
-	return True;
+	if tmp == []:
+		return True
+	else:
+		return False
 
-def caesar_list (word, key):
-	pass
+def caesar_list (word, key = [1, 2, 3]):
+	alphabet = list (string.ascii_lowercase)
+	for char in word:		# Check if all chars from word are in ascii lowercse alphabet
+		if char not in alphabet:
+			raise ValueError
+	newWord = ""
+	count = 0
+	keyLength = len (key)
+	for char in word:
+		newWord += alphabet[(alphabet.index (char) + key[count % keyLength]) % 26]		# To string newchar (in first cycle blank) append shift char from alphabet
+		count += 1
+	return newWord
 
 def caesar_varnumkey (key):
 	pass
 
-#print (balanced_paren ("{1<2(>3)}"))
+print (balanced_paren ("{("))
+#caesar_list ("")
